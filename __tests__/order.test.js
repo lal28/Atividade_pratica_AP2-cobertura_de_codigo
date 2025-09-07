90e69d9f744210d687d483db7f0b7db8e2ed5151
Pedro9185 <pedrocatarina.ip@gmail.com>
2025-09-07 20:06:06 -0300

    add: adicionados testes 8,9e10

--- __tests__/order.test.js
+++ __tests__/order.test.js
@@ -8,7 +8,7 @@ describe("Teste 1:  criar item", () => {
   test("deve criar um pedido vazio", () => {
     // Cria um pedido simples
     const pedido = new Order(1);
-    
+
     // Verifica se foi criado corretamente
     expect(pedido.id).toBe(1);
     expect(pedido.items).toEqual([]); // Lista vazia
@@ -20,11 +20,11 @@ describe("Teste 1:  criar item", () => {
 //TESTE 2: Classe Item
 describe("Teste 2: criar Item", () => {
   test("deve criar um item", () => {
-    const pizza = new Item(1, "Pizza", 25.50);
-    
+    const pizza = new Item(1, "Pizza", 25.5);
+
     expect(pizza.id).toBe(1);
     expect(pizza.name).toBe("Pizza");
-    expect(pizza.price).toBe(25.50);
+    expect(pizza.price).toBe(25.5);
   });
 });
 
@@ -32,12 +32,12 @@ describe("Teste 2: criar Item", () => {
 describe("Teste 3: Adicionar Item", () => {
   test("deve adicionar item ao pedido", () => {
     const pedido = new Order(1);
-    const pizza = new Item(1, "Pizza", 25.50);
-    
+    const pizza = new Item(1, "Pizza", 25.5);
+
     pedido.addItem(pizza);
-    
+
     expect(pedido.items.length).toBe(1);
-    expect(pedido.total).toBe(25.50);
+    expect(pedido.total).toBe(25.5);
   });
 });
 
@@ -45,13 +45,50 @@ describe("Teste 3: Adicionar Item", () => {
 describe("Teste 4: Remover Item", () => {
   test("deve remover item do pedido", () => {
     const pedido = new Order(1);
-    const pizza = new Item(1, "Pizza", 25.50);
-    
+    const pizza = new Item(1, "Pizza", 25.5);
+
     pedido.addItem(pizza);
-    pedido.removeItem(1);  // Remove pizza
-    
+    pedido.removeItem(1); // Remove pizza
+
     expect(pedido.items.length).toBe(0);
     expect(pedido.total).toBe(0);
   });
 });
 
+//TESTE 8: Completar Pedido - Erro
+describe("Teste 8: Completar Pedido - Erro", () => {
+  test("deve dar erro ao completar pedido não pago", () => {
+    const pedido = new Order(1); // Status = "created"
+
+    // Tenta completar sem pagar - deve dar erro
+    expect(() => pedido.complete()).toThrow(
+      "Order must be paid before it can be completed"
+    );
+  });
+});
+
+//TESTE 9: Cancelar Pedido - Sucesso
+describe("Teste 9: Cancelar Pedido - Sucesso", () => {
+  test("deve cancelar pedido criado", () => {
+    const pedido = new Order(1); // Status = "created"
+
+    pedido.cancel(); // Deve funcionar
+
+    expect(pedido.status).toBe("cancelled");
+  });
+});
+
+//TESTE 10: Cancelar Pedido completado
+
+describe("Teste 10: Cancelar Pedido completado - Erro", () => {
+  test("deve dar erro ao cancelar pedido completado", () => {
+    const pedido = new Order(1);
+    pedido.pay();
+    pedido.complete(); // Status = "completed"
+
+    // Tenta cancelar pedido completado - deve dar erro
+    expect(() => pedido.cancel()).toThrow(
+      "Completed order cannot be cancelled"
+    );
+  });
+});
